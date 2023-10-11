6444029676f64ad210bc8ccd00503123eba1646e
GhadaGK <143323804+GhadaGK@users.noreply.github.com>
2023-10-11 16:04:23 +0300

    hacker_rank_subarray_division_1_c++.cpp

---
+++ hacker_rank_subarray_division_1_c++.cpp
@@ -0,0 +1,12 @@
+int birthday(vector<int> s, int d, int m) {
+    int count=0;
+    for(int i = 0 ; i < s.size() ; i++){
+        int sum = 0 ;
+        for(int j = i ; j < m + i ; j++ ){
+            if(j == s.size())break;
+            sum+= s[j];
+        }
+        if(sum == d) count++;
+    }
+    return count;
+}
