a9e6514fe000d73fa981e5ab858c97e30d9f9e0a
GhadaGK <143323804+GhadaGK@users.noreply.github.com>
2024-01-11 15:35:35 +0200

    leetcode longest substring without repeating characters O(n^3)

---
+++ leetcode_longest_substring_without_repeating_characters_O(n^3).cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+bool checkReaptation(string& s, int start, int end ){
+    vector<int> chars(128);
+    while(start <= end){
+        char c = s[start];
+        chars[c]++;
+        if(chars[c] > 1){
+            return false;
+        }
+        start++;
+    }
+    return true;
+}
+
+int lengthOfLongestSubstring(string s){
+    int n = s.length();
+    int result = 0;
+    for(int i = 0 ; i < n ; i++){
+        for(int j = i ; j < n ; j++ ){
+            if(checkReaptation(s, i, j)){
+                result = max(result, j-i+1);
+            }
+        }
+    }
+    return result;
+}
+int main() {
+    string s = "abcabcbb";
+    cout << lengthOfLongestSubstring(s);
+
+    return 0;
+}
